ee4b5e42d76db3113cbcdeb27a1f8ea2957831e1
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-25 20:35:33 -0700

    Created a wait_for_selectorrs() function to loop through the css selectors and once that specific css selector that's been passed a parameter is found, then it will be added to the element list and returns it.

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ tops.click()
 #ADDING A DELAY SO THAT THE WEBSITE DOES NOT THINK I AM A BOT
 time.sleep(1)
 
+'''
+THIS FUNCTION WILL RUN A WHILE LOOP BY LOOKING AT THE CSS_SELECTOR AS A PARAMETER, ONCE IT FINDS THAT SPECIFIC
+CSS SELECTOR, THEN IT WILL ADD IT TO THE ELEMENT LIST AND RETURNS THE ELEMENT.
+'''
+def wait_for_selectors(css_selector):
+    elements = []
+    while len(elements) == 0:
+        elements = driver.find_elements_by_css_selector(css_selector)
+        time.sleep(0.00001)
+    return elements
+
 
 search_term = "ENTER THE DESIRED PRODUCT/ITEM" #e.g. Box Logo
 style = "ENTER THE DESIRED PRODUCT/ITEM'S COLOR" #e.g. White
