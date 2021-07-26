e7e8fa9b9a5614e6c6c28ff07e3ebe5ec67cf990
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-25 20:25:37 -0700

    Enter the desired product's name and color in two separate variables. Access the list of product names in the T-shirt category.

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ driver.get("https://www.supremenewyork.com/shop/all")
 #I WANT TO BUY ONE OF THEIR T-SHIRTS. FEEL FREE TO CHANGE PATH DEPENDING ON WHAT PRODUCT YOU WANT TO BUY.
 tops = driver.find_element_by_xpath('//*[@id="nav-categories"]/li[5]/a')
 tops.click()
-
+#ADDING A DELAY SO THAT THE WEBSITE DOES NOT THINK I AM A BOT
 time.sleep(1)
+
+
+search_term = "ENTER THE DESIRED PRODUCT/ITEM" #e.g. Box Logo
+style = "ENTER THE DESIRED PRODUCT/ITEM'S COLOR" #e.g. White
+#LIST OF ITEMS IN THE T-SHIRT CATEGORY.
+items = driver.find_elements_by_css_selector('li a.name-link')
